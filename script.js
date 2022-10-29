1a3c9de9b8e4d0c13e2cac2c654d94293de81c7c
JonasP <visokiems@gmail.com>
2022-10-29 10:29:23 +0300

    code added

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget > 100000000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let avatar = new Movie("Avatar", "Cameron", 500000000);
+
+console.log(avatar.wasExpensive());
